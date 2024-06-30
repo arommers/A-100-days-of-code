eb21f7b5a33a594675f2c62cd5794119cd4a4d38
arommers <arommers@student.codam.nl>
2024-06-30 17:06:43 +0200

    working on pong

--- pong/main.py
+++ pong/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.title("Pong")
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 left_paddle = Paddle(-350)
 right_paddle = Paddle(350)
+ball = Ball()
 
 screen.listen()
 screen.onkey(left_paddle.move_up, "w")
@@ -19,6 +22,13 @@ screen.onkey(right_paddle.move_down, "Down")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
